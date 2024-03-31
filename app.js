87ba66fd56ddc3cae282450e9cff9757670018c2
Shahul-TechWizard <shahul.techwizard@gmail.com>
2024-03-31 21:14:57 +0530

    scratch v1

--- app.js
+++ app.js
@@ -33,4 +33,5 @@ form.addEventListener("submit", (e) => {
     //   window.location.reload();
     // })
     .catch((error) => console.error("Error!", error.message));
+  alert("Thank you! your form is submitted successfully.");
 });
