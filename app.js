b93dfd7551bf1478daa2ab23ceb503b312614375
Shahul-TechWizard <shahul.techwizard@gmail.com>
2024-03-31 21:02:49 +0530

    scratch v1

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const scriptURL =
   "https://script.google.com/macros/s/AKfycbzawg7BGsyNJXJT8d8ZErb13VHvtNlYgSzhUm9mz4O62ZQ-kqsIML94yB3o1e1thk9f/exec";
 
 const form = document.forms["contact-form"];
-
+var submissionDateTime = new Date().toLocaleString();
+document.getElementById("Timestamp").value = submissionDateTime;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
@@ -31,7 +32,4 @@ form.addEventListener("submit", (e) => {
     //   window.location.reload();
     // })
     .catch((error) => console.error("Error!", error.message));
-
-  var submissionDateTime = new Date().toLocaleString();
-  document.getElementById("Timestamp").value = submissionDateTime;
 });
