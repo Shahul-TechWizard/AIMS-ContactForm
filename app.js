8420bbd886a13e59718d6144bd3af2b0f4361c27
Shahul-TechWizard <shahul.techwizard@gmail.com>
2024-04-02 22:34:06 +0530

    v1

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 const scriptURL =
-  "https://script.google.com/macros/s/AKfycbzawg7BGsyNJXJT8d8ZErb13VHvtNlYgSzhUm9mz4O62ZQ-kqsIML94yB3o1e1thk9f/exec";
+  "https://script.google.com/macros/s/AKfycbxTCi_pL5D5VbCzFq9IkQBMMXGNXqnMAdrYC7KccpBLtOKpcYMaZtHApt3KJr2urIXA/exec";
 
 // var submissionDateTime = new Date().toLocaleString();
 // document.getElementById("submissionDateTime").value = submissionDateTime;
